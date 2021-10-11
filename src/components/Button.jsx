f2fefc8c530ec553ead7fa22bc4de8f0db5a8e0e
sahil-shubham <sahil.shubham2000@gmail.com>
2021-10-12 00:27:48 +0530

    added register button next to hackathon heading

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Button = styled.a`
+  background-color: #25c2a0;
+  color: #fff;
+  font-size: 1rem;
+  font-weight: 600;
+  font-family: inherit;
+  cursor: pointer;
+  border: #25c2a0;
+  padding: 0.5rem 1rem;
+  border-radius: 7px;
+
+  &:hover{
+    color: #fff;
+  }
+`;
+
+export default Button;
